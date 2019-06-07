8d56cee3321d042c838009bf7f8c10a9e2669c2a
Noboru Katayama <katayama@rs.tus.ac.jp>
2019-06-07 15:07:18 +0900

    mel

--- recog.py
+++ recog.py
@@ -7,6 +7,10 @@ import argparse
 import matplotlib.pyplot as plt
 from random import randint
 from sklearn.model_selection import train_test_split
+from python_speech_features import mfcc
+from scipy.fftpack import fft
+import librosa
+import librosa.display
 
 import wave
 import pyaudio
@@ -17,7 +21,7 @@ def main():
     parser.add_argument('-p', '--predict', action='store_true', help='Predict')
     parser.add_argument('-d', '--datadir', type=str, default='data', help='Data dir to learn/predict')
     parser.add_argument('--epochs', type=int, default=100, help='Number of epochs')
-    parser.add_argument('--batch_size', type=int, default=256, help='Batcn size')
+    parser.add_argument('--batch_size', type=int, default=512, help='Batcn size')
 
     args = parser.parse_args()
 
@@ -33,6 +37,22 @@ def main():
         predict(args, options)
     else:
         learn(args, options)
+
+def calculate_melsp(x, n_fft=1024, hop_length=128):
+    stft = np.abs(librosa.stft(x, n_fft=n_fft, hop_length=hop_length))**2
+    log_stft = librosa.power_to_db(stft)
+    melsp = librosa.feature.melspectrogram(S=log_stft,n_mels=128)
+    return melsp
+
+# display wave in heatmap
+def show_melsp(melsp):
+    librosa.display.specshow(melsp)
+    plt.colorbar()
+    plt.show()
+
+def show_wave(x):
+    plt.plot(x)
+    plt.show()
 
 def learn(args, options):
     epochs = args.epochs
@@ -58,13 +78,31 @@ def learn(args, options):
 
             for i in range(chunk_num):
                 pos = randint(0, wav_length - chunk_length)
-                X.append(x[pos:pos+chunk_length])
+                x_ =  x[pos:pos+chunk_length]
+
+                # x_mfcc = mfcc(x[pos:pos+chunk_length], samplerate=options["rate"])
+                # X.append(x_mfcc[0])
+                # X.append(x[pos:pos+chunk_length])
+
+                # x_ = abs(fft(x[pos:pos+chunk_length]))
+                # X.append(x_[0:512])
+
+                melsp = calculate_melsp(x_)
+                X.append(melsp)
+                
+                # print("wave size:{0}\nmelsp size:{1}".format(x.shape, melsp.shape))
+                # show_wave(x)
+                # show_melsp(melsp)
+
+                # plt.figure(figsize=(15,3))
+                # plt.plot(x)
+                # plt.show()
+
                 Y.append(mode_idx)
 
+
+
     X = np.array(X).astype('float32')
-    # plt.figure(figsize=(15,3))
-    # plt.plot(X.T)
-    # plt.show()
 
     Y = tf.keras.utils.to_categorical(Y, mode_num)
 
